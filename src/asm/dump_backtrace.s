1294ff25542d557fa9ac690df35057590dbb06c7
eddyia1 <eddyia@oregonstate.edu>
2025-06-08 20:44:11 -0700

    Update dump_backtrace.s

--- src/asm/dump_backtrace.s
+++ src/asm/dump_backtrace.s
@@ -1,21 +1,27 @@
 .globl dump_backtrace
 .type dump_backtrace, @function
 dump_backtrace:
+        /* Set up stack */
         push %rbp
         mov %rsp, %rbp
 
+        /* rbx will keep track of the current rbp position */
         mov %rbp, %rbx
 loop:
+        /* Move the ret address of the current function frame into rdi */
         mov 8(%rbx), %rdi
         call _dump_backtrace
 
+        /* Check if the next depth is valid */
         test (%rbx), %rbx
         jz done
 
+        /* Dereference the current ret address and go to the next frame */
         mov (%rbx), %rbx
         jmp loop
 
-done:
+done:       
+        /* Epilog */
         mov %rbp, %rsp
         pop %rbp
         ret
