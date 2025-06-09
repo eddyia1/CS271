6497c7525797dce464116b31f66e6f340d439ac5
eddyia1 <eddyia@oregonstate.edu>
2025-06-08 20:45:12 -0700

    Update dump_registers.s

--- src/asm/dump_registers.s
+++ src/asm/dump_registers.s
@@ -4,7 +4,7 @@
 .globl dump_registers
 .type dump_registers, @function
 dump_registers:
-        //Push all register values to the stack
+        /* Push all register values to the stack */
         push %rax
         push %rbx
         push %rcx
@@ -13,8 +13,8 @@ dump_registers:
         push %rdi
         push %rbp
 
-        //The original value of %rsp before pushing to the stack is equal to
-        //an offset of 8 * (Number of pushes + 1).
+        /* The original value of %rsp before pushing to the stack is equal to */
+        /* an offset of 8 * (Number of pushes + 1). */
         leaq (8*8)(%rsp), %rax
 
         push %rax
@@ -27,14 +27,14 @@ dump_registers:
         push %r14
         push %r15
 
-        //Pass the stack pointer into rdi to allow C to iterate
-        //over it and print the value
+        /* Pass the stack pointer into rdi to allow C to iterate */
+        /* over it and print the value */
         movq %rsp, %rdi
 
-        //Call the externally-linked C function defined in debug.c
+        /* Call the externally-linked C function defined in debug.c */
         call _debug_dump_registers
 
-        //Restore the original stack position 
+        /* Restore the original stack position */
         leaq (16*8)(%rsp), %rsp
 
         ret
